5b88a2c5ab4d37f105b602b1cd33bdc4e2c16196
l00fi <yura.kabantsov@gmail.com>
2025-02-28 11:51:03 +0300

    Раскомментил команду kill

--- 1lab/main.py
+++ 1lab/main.py
@@ -1,5 +1,5 @@
 # main.py будет клиентом, который обращается к server.py
-#-------------------------------------------------------
+# -------------------------------------------------------
 import socket
 import os
 import readline
@@ -11,21 +11,23 @@ NAME = 'localhost'
 PORT = 9090
 FORMAT = 'utf-8'
 PACKAGE_LEN = 8
-JSON_NAME = 'data_server'
 COMMANDS = {
     "get tasklist": "Выводит список процессов исполняемых на сервере",
     "exit": "Прерывает соединение с сервером",
-    "cls": "Очистить консоль" #,
-    # "kill [PID]": "Остановка процесса по его PID",
+    "cls": "Очистить консоль",
+    "kill [PID]": "Остановка процесса по его PID"
 }
+
 
 def get_action_time():
     return time.strftime("%Y-%m-%d_%H-%M-%S", time.gmtime())
+
 
 def reciver(s):
     package = s.recv(PACKAGE_LEN, socket.MSG_WAITALL)
     n = struct.unpack('!Q', package)[0]
     return 0 if n == 0 else s.recv(n, socket.MSG_WAITALL)
+
 
 def sender(client, message):
     package = struct.pack('!Q', len(message)) + message
@@ -33,9 +35,11 @@ def sender(client, message):
     while sended_len < len(package):
         sended_len += client.send(package[sended_len:])
 
+
 def completer(inp, state):
     options = [command for command in COMMANDS if command.startswith(inp)]
     return options[state] if state < len(options) else None
+
 
 def client_command_input(client_socket):
     readline.set_completer(completer)
@@ -48,34 +52,35 @@ def client_command_input(client_socket):
         for command in COMMANDS:
             print(f"{command} | {COMMANDS[command]}")
         return 'inner command'
-    
+
     elif message == 'exit':
 
         return 'inactive'
-    
+
     elif message == 'cls':
 
         if os.name == 'posix':
             os.system('clear')
-        else:    
+        else:
             os.system('cls')
         return 'inner command'
-    
+
     elif message == 'get tasklist':
-        
+
         sender(client_socket, message.encode(FORMAT))
         sender(client_socket, b'')
         return 'reciver'
-    
-    # elif 'kill' in message:
 
-    #     sender(client_socket, message.encode(FORMAT))
-    #     sender(client_socket, b'')
-    #     return 'inner command'
-    
+    elif 'kill' in message:
+
+        sender(client_socket, message.encode(FORMAT))
+        sender(client_socket, b'')
+        return 'inner command'
+
     else:
         print("Error! Command does not exist")
         return 'incorrect command'
+
 
 def main():
     client = socket.socket()
@@ -87,7 +92,7 @@ def main():
 
             if client_status == 'incorrect command':
                 continue
-            
+
             if client_status == 'reciver':
                 data = reciver(client)
                 file_name = get_action_time()
@@ -107,6 +112,6 @@ def main():
     except Exception as e:
         print(e)
 
+
 if __name__ == "__main__":
     main()
-
--- 1lab/server.py
+++ 1lab/server.py
@@ -11,6 +11,7 @@ FORMAT = 'utf-8'
 PACKAGE_LEN = 8
 JSON_NAME = 'data_server'
 
+
 def get_action_time():
     return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 
@@ -19,6 +20,7 @@ def reciver(s):
     package = s.recv(PACKAGE_LEN, socket.MSG_WAITALL)
     n = struct.unpack('!Q', package)[0]
     return 0 if n == 0 else s.recv(n, socket.MSG_WAITALL)
+
 
 def sender(client, message):
     package = struct.pack('!Q', len(message)) + message
@@ -32,7 +34,7 @@ def converter(table):
     def create_file():
         with open(f'{JSON_NAME}.json', 'a') as f:
             f.write(json.dumps(table))
-    
+
     if os.path.exists(f'{JSON_NAME}.json'):
         os.remove(f'{JSON_NAME}.json')
         create_file()
@@ -55,9 +57,10 @@ def main():
             command = reciver(conn)
             if command != 0:
                 command = command.decode(FORMAT)
-                
+
                 if 'get tasklist' in command:
-                    print(f'[{get_action_time()}] executed the command: "get tasklist"')
+                    print(
+                        f'[{get_action_time()}] executed the command: "get tasklist"')
 
                     if os.name == 'posix':
                         table = os.popen("ps").read()
@@ -67,21 +70,21 @@ def main():
                     converter(table)
                     sender(conn, table.encode(FORMAT))
                     sender(conn, b'')
+                    
                 # Не даёт права
-                # if 'kill' in command:
-                #     if os.name == 'posix':
-                #         pass
-                #     else:
-                #         pid = command.split()[1]
-                #         os.popen(f"taskkill /pid {pid}")
-                #         print(f"[{get_action_time()}] process killed: {pid}")
-            
+                if 'kill' in command:
+                    if os.name == 'posix':
+                        pass
+                    else:
+                        pid = command.split()[1]
+                        os.popen(f"taskkill /pid {pid}")
+                        print(f"[{get_action_time()}] process killed: {pid}")
+
         conn.close()
 
     except Exception as e:
         print(e)
 
+
 if __name__ == "__main__":
     main()
-
-
