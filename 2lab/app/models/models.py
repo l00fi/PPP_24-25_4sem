6dfae717dc790fddbe727e65f6b519374dd9f1a6
l00fi <yura.kabantsov@gmail.com>
2025-03-21 09:41:32 +0300

    Осталась последняя базовая фича

--- 2lab/app/models/models.py
+++ 2lab/app/models/models.py
@@ -11,4 +11,4 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
-    token = Column(String)
+    # token = Column(String)
--- 2lab/main.py
+++ 2lab/main.py
@@ -6,6 +6,9 @@ from app.schemas.schemas import User
 import secrets
 # Работаем с бд
 import sqlite3
+# Запуск сервера с заданным ip и портом
+import uvicorn
+from app.api.endpoints import FastApiServerInfo 
 
 # fastAPI 
 app = FastAPI()
@@ -16,7 +19,7 @@ async def root():
     return {"message": "Hello World"}
 
 # Логинимся и проверяем, есть ли такой
-@app.post("/sign-up/")
+@app.post(FastApiServerInfo.SIGN_UP_ENDPOINT)
 async def sign_up(user: User):
     token, id = None, None
     # Словарь для возврата в return
@@ -24,32 +27,32 @@ async def sign_up(user: User):
     # Соезинение с бд
     connection = sqlite3.connect("app/db/database.db")
     cursor = connection.cursor()
-    # Запрос на поиск ползователя по паролю и почте
+    # Запрос на поиск пользователя по почте
     cursor.execute("SELECT * FROM Users WHERE email = ?", 
                    (user.email,))
     rows = cursor.fetchall()
     if rows:
-        # Прохожу по всем найденным и записываю их в existing_users
-        for row in rows:
-            id, email, password, token = row
-            existing_user = {
-                "id": id,
-                "email": email,
-                "token": token  
-            }
-            existing_users[id] = existing_user
+        # # Прохожу по всем найденным и записываю их в existing_users
+        # for row in rows:
+        #     id, email, password = row
+        #     existing_user = {
+        #         "id": id,
+        #         "email": email,
+        #     }
+        #     existing_users[id] = existing_user
+        pass
     else:
-        # Делаю токен
-        token = secrets.token_urlsafe()
         # Запрос на добавление новую строку 
-        cursor.execute("INSERT INTO Users (email, password, token) VALUES (?, ?, ?)", 
-                    (user.email, user.password, token))
+        cursor.execute("INSERT INTO Users (email, password) VALUES (?, ?)", 
+                    (user.email, user.password))
         # Добавляю в словарь для return
         connection.commit()
         #Получаю id
         cursor.execute("SELECT id FROM Users WHERE email = ? AND password = ?", 
                     (user.email, user.password))
         id = cursor.fetchall()[0][0]
+        # Получаю токен
+        token = secrets.token_urlsafe()
         # Записываю в existing_users
         existing_users[id] = {
                 "id": id,
@@ -59,3 +62,38 @@ async def sign_up(user: User):
         
     connection.close()
     return existing_users
+
+@app.post(FastApiServerInfo.LOGIN_ENDPOINT)
+async def login(user: User):
+    token, id = None, None
+    # Словарь для возврата в return
+    existing_users = dict()
+    # Соезинение с бд
+    connection = sqlite3.connect("app/db/database.db")
+    cursor = connection.cursor()
+    # Запрос на поиск пользователя по паролю и почте
+    cursor.execute("SELECT * FROM Users WHERE email = ? AND password = ?", 
+                   (user.email,user.password))
+    # Такой пользователь всегда один
+    rows = cursor.fetchone()
+    if rows:
+        # Распаковываю данные о польщователе
+        id, email, password = rows
+        # Генерирую токен
+        token = secrets.token_urlsafe()
+        # Добавляю ответ
+        existing_users[id] = {
+                "id": id,
+                "email": email,
+                "token": token  
+            }
+    else:
+        # Если введн не первый password, то возвращаю сообщение об ошибке
+        connection.close()
+        return {"Message": "Incorrect password"}
+    
+    connection.close()
+    return existing_users
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=FastApiServerInfo.IP, port=FastApiServerInfo.PORT)
